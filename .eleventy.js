cf5e3212e9127b436c6fecee701b88d164c71756
Kevin Schmincke <kschmincke@asymmetrik.com>
2021-07-15 09:46:50 -0400

    adjust assets passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass/');
 
-  eleventyConfig.addPassthroughCopy('./src/assets/me.jpg');
+  eleventyConfig.addPassthroughCopy('./src/assets');
 
   eleventyConfig.addCollection('posts', (collection) => {
     return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
